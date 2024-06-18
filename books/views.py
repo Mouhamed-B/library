47f384634f20db99dff516c2f61e6e2d79f0b02c
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-06-18 14:08:57 +0000

    add views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,39 @@
+from typing import Any
 from django.shortcuts import render
-
+from django.views.generic import FormView, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import *
 # Create your views here.
+
+class BookListView(ListView):
+    model = Book
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_is_authenticated'] = self.request.user.is_authenticated
+        return context
+
+
+class BookDetailView(DetailView):
+    model = Book
+    
+
+class BookFormView(LoginRequiredMixin,FormView):
+    login_url = '/sign-in/'
+    template_name = 'books/create_book.html'
+    form_class = BookForm
+    success_url = 'books/create_book?success=true'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        print(form)
+        form.save()
+        return super().form_valid(form)
+
+
+class SignInView(LoginView):
+    template_name = 'books/sign_in.html'
+    next_page = '/'
+    redirect_authenticated_user = True
