684706cd8220655af295438a0772d7b26b30c57c
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-06-18 14:07:50 +0000

    book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Book(models.Model):
+    STATUSES = [
+        ('AVAILABLE','AVAILABLE'),
+        ('CHECKED_OUT','CHECKED_OUT')
+    ]
+
+    title = models.CharField(max_length=127, blank=False, null=False)
+    description = models.CharField(blank=False, null=False)
+    image = models.URLField(blank=False, null=False)
+    checked_out_by = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, default=None)
+
+    @property
+    def status(self):
+        return 'Available' if self.checked_out_by is None else 'Checked Out'
